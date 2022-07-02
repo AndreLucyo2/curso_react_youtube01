a1130d6912267b49f5c21c93c745531d47e3f596
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-02 17:10:58 -0300

    video 11 Mesmo evendo para componentes diferentes:

--- meu-projeto-react01/src/components/Evento.js
+++ meu-projeto-react01/src/components/Evento.js
@@ -8,12 +8,19 @@ function Evento() {
         console.log('Ativando primeiro evento');
         alert(`Ativando primeiro evento!`);
     }
+
+    //Mesmo evendo para componentes diferentes:
+    function segundoEvento(){
+        console.log('Ativando segundo evento');
+        alert(`Ativando segundo evento!`);
+    }
           
     //Retorno o evento ---------------------------------------
     return (
         <div>
             <p>Clique aqui para disparar um evento</p>
             <Button event={meuEvento} text="Primeiro evendo"/>
+            <Button event={segundoEvento} text="Segundo evendo"/>
         </div>
     )
 
