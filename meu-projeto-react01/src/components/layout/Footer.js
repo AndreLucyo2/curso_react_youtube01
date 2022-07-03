71c96413319ebbd140867b4ff9dbe7c0c58b426d
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-03 20:10:10 -0300

    video 16 aplicando icons

--- meu-projeto-react01/src/components/layout/Footer.js
+++ meu-projeto-react01/src/components/layout/Footer.js
@@ -1,10 +1,22 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
+import styles from './Footer.module.css'
 
-function Footer()
-{
-    return(
+function Footer() {
+    return (
         <footer>
-            <h3>RODAPÉ!!!!</h3>
+            <ul className={styles.social_list}>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+            </ul>
+            <p>Nosso Rodapé</p>
         </footer>
     )
 }
