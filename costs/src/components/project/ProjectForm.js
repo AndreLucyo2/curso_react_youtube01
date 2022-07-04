2cc81ffba776ed9cb768e3b8d239675866165cd2
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-04 12:56:01 -0300

    video 21 criado estrutura da pagina projeto

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,9 +1,22 @@
-function ProjectForm(){
+function ProjectForm() {
 
-    return(
-        <p>
-            Criando formulario do projeto
-        </p>
+    return (
+        <form>
+            <div>
+                <input type="text" placeHolder="Insira o nome do projeto" />
+            </div>
+            <div>
+                <input type="number" placeHolder="Insira o orçamento total" />
+            </div>
+            <div>
+                <select name="category_id">
+                    <option disable>Selecione a categoria</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value="Criar Projeto"/>
+            </div>
+        </form>
     )
 }
 
