6361e4379f1ec4e75f6473320fd7a2cc45fc3f3a
AndreLucyo <andre.lucio@nextar.com.br>
2022-07-10 13:52:53 -0300

    video 24 salvando dados no json api fake

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,12 +1,43 @@
+//Substitua "import { useHistory }" por "import { useNavigate }"
+import { useNavigate  } from 'react-router-dom'
+
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject() {
+
+  //permite fazer redirects, redireciona para outras paginas
+  const history = useNavigate();
+
+  function createPost(project) {
+
+    // initialize cost and services
+    project.cost = 0;
+    project.services = [];//adiciona ou remove novos serviços - deve ficar no backend
+
+    fetch('http://localhost:5000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      //manda os dados por post para o servidor
+      body: JSON.stringify(project),
+
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        //faz o redirect:
+        history('/projects', { message: 'Projeto criado com sucesso!' })
+      })
+  }
+
   return (
     <div className={styles.newproject_container} >
       <h1>Criar projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText="Criar projeto"/>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
   )
 }
--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -6,9 +6,11 @@ import SubmitButton from '../form/SubmitButton'
 
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({ btnText }) {
-
+function ProjectForm({ handleSubmit, btnText, projectData }) {
     const [categories, setCategories] = useState([]);
+
+    //se vem os dados do projeto, usa eles , caso contrario iniciar objeto vazio
+    const [project, setProject] = useState(projectData || {});
 
     //buscar as categorias da api fake:
     useEffect(() => {
@@ -17,32 +19,68 @@ function ProjectForm({ btnText }) {
             headers: {
                 'Content-Type': 'application/json',
             },
-        }).then((resp) => resp.json()).then((data) => {
-            setCategories(data);
-        }).catch((erro) => console.log(erro))
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data);
+            }).catch((erro) => console.log(erro))
     }, [])
+
+    //metodo que submete o form: https://youtu.be/gIWmB3EV4Bo?list=PLnDvRpP8BneyVA0SZ2okm-QBojomniQVO&t=468
+    const submit = (e) => {
+        //nao usa o pagereload
+        e.preventDefault();
+        //Executa o metodo recebido pela prope passa o projeto como argumento
+        handleSubmit(project);
+    }
+
+    //passa os dados para os inputs: metodo dinamico para refletir na tela as alterações no imput
+    function handleChange(e) {
+        //cria um novo projeto:
+        setProject({ ...project, [e.target.name]: e.target.value });
+        //console.log(project)
+    }
+
+    //metodo dinamico para selects e refletir na tela
+    function handleCategory(e) {
+        //cria um novo projeto:
+        setProject({
+            ...project, category: {
+                id: e.target.value,
+                name: e.target.options[e.target.selectedIndex].text,
+            },
+        })
+        console.log(project);
+
+    }
 
 
     return (
-        <form className={styles.form}>
+        <form onSubmit={submit} className={styles.form}>
             <Input
                 type="text"
                 text="Nome do projeto"
                 name="name"
                 placeholder="Insira o nome do projeto"
+                handleOnChange={handleChange}
+                value={project.name ? project.name : ''}
             />
 
             <Input
                 type="number"
-                text="Nome do projeto"
+                text="Orçamento do projeto"
                 name="budget"
                 placeholder="Insira o orçamento total"
+                handleOnChange={handleChange}
+                value={project.budget ? project.budget : ''}
             />
 
             <Select
                 text="Selecione a categoria"
                 name="category_id"
                 options={categories}
+                handleOnChange={handleCategory}
+                value={project.category ? project.category.id : ''}
             />
 
             <SubmitButton text={btnText} />
